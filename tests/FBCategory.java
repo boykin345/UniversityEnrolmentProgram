864bde323160c852d8893fd226dac162933b630a
Abdulaziz Sarkhoh <efyas13@exmail.nottingham.ac.uk>
2023-05-15 16:40:07 +0100

    Upload New File

---
+++ tests/FBCategory.java
@@ -0,0 +1,70 @@
+import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+
+import org.junit.Test;
+
+
+
+public class FBCategoryTest {
+
+	@Test
+	public void setCategoryBudgetTest() {
+		// Create an instance of the class being tested
+		FBCategory category = new FBCategory();
+
+        // Set the input value for testing
+        BigDecimal expectedValue = new BigDecimal("100.01");
+
+        // Call the method under test
+        category.setCategoryBudget(expectedValue);
+
+        // Verify the expected result
+        assertEquals(expectedValue , category.CategoryBudget());
+        
+        
+    }
+	
+	@Test
+	public void addExpenseTest() {
+		// Create an instance of the class being tested
+		FBCategory category = new FBCategory();
+		
+		category.addExpense(new BigDecimal("100.00"));
+		
+		BigDecimal previousSpend = category.CategorySpend();
+		
+        BigDecimal addingValue = new BigDecimal("100.01");
+
+        // Call the method under test
+        category.addExpense(addingValue);
+        
+        // Verify the expected result
+        assertEquals( previousSpend.add(addingValue), category.CategorySpend());
+        
+        
+    }
+	
+	
+	@Test
+	public void removeExpenseTest() {
+		// Create an instance of the class being tested
+		FBCategory category = new FBCategory();
+		
+		category.addExpense(new BigDecimal("100.00"));
+		
+		BigDecimal previousSpend = category.CategorySpend();
+		
+        BigDecimal removingValue = new BigDecimal("5");
+
+        // Call the method under test
+        category.removeExpense(removingValue);
+        
+        // Verify the expected result
+        assertEquals( previousSpend.subtract(removingValue), category.CategorySpend());    
+        
+    }
+	
+		
+	
+}
